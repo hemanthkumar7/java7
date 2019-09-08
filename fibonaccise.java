4f008610630a71167a22609925f7aab10957f441
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2019-09-08 16:35:03 +0530

    Create fibonaccise.java

---
+++ fibonaccise.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Fibonacci
+{
+public static void main(String args[])
+{
+    //program to print first n fibonacci numbers where n is taken as input from user
+    System.out.println("Enter the number of Fibonacci numbers you want: ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    fibonacci(n);
+    
+   } 
+
+static void fibonacci(int n)
+{
+
+    int counter=0,i=1,j=0,prev;
+    System.out.println("\n\n\n");
+
+    while(counter<n) 
+    {
+        System.out.println(i);
+        prev=i;
+        i=i+j;
+        j=prev;
+        counter++;
+    }
+}
+
